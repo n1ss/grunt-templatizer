d7e9573270b2975b44cf552682a9777db6829c4e
Boris Morachev <boris.morachev@kruschecompany.com>
2015-04-02 00:01:55 +0300

    something is wrking already

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,21 @@
-/*
- * grunt-templatizer
- * https://github.com/n1ss/grunt-templatizer
- *
- * Copyright (c) 2015 Boris Morachev
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
+    templatizer: {
+      options: {
+      },
+      target: {
+        files: [{
+            expand: true,
+            cwd: 'test',
+            src: ['**/*.jade'],
+            dest: 'test/result', 
+            ext: '.js'
+        }]
+      }
+    },
     jshint: {
       all: [
         'Gruntfile.js',
@@ -22,52 +26,19 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
-
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp']
     },
-
-    // Configuration to be run (and then tested).
-    templatizer: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
-
-    // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js']
     }
-
   });
 
-  // Actually load this plugin's task(s).
+  
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'templatizer', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+  grunt.registerTask('default', ['jshint', 'clean', 'templatizer']);
 };
